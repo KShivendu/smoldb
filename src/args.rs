a19555802daf553d726f28ac1e7efb0f7e7cbfa9
Kumar Shivendu <kshivendu1@gmail.com>
2025-07-19 23:40:56 +0530

    Improve arg parsing (#40)

--- src/args.rs
+++ src/args.rs
@@ -1,17 +1,25 @@
 use clap::Parser;
 use http::Uri;
 
+fn parse_uri(s: &str) -> Result<Uri, http::uri::InvalidUri> {
+    if s.contains("://") {
+        s.parse::<Uri>()
+    } else {
+        format!("http://{s}").parse::<Uri>()
+    }
+}
+
 #[derive(Parser)]
 #[clap(version, about)]
 pub struct Args {
     /// Url of the bootstrap node
-    #[clap(short, long)]
+    #[clap(short, long, value_parser = parse_uri)]
     pub bootstrap: Option<Uri>,
     /// Url of the node
-    #[clap(short, long, default_value = "http://0.0.0.0:9000")]
+    #[clap(short, long, default_value = "0.0.0.0:9000", value_parser = parse_uri)]
     pub url: Uri,
     /// Url of the node
-    #[clap(short, long, default_value = "http://0.0.0.0:5000")]
+    #[clap(short, long, default_value = "0.0.0.0:5000", value_parser = parse_uri)]
     pub p2p_url: Uri,
     /// Peer id
     #[clap(long)]
