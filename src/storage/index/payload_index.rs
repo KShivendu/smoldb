d48f5baf4c1d32517df4f6d905e518340a7fe228
KShivendu <kshivendu1@gmail.com>
2025-08-02 14:07:44 +0530

    working index 😎

--- src/storage/index/payload_index.rs
+++ src/storage/index/payload_index.rs
@@ -3,8 +3,12 @@ use serde_json::Value;
 use sled::Db;
 use std::collections::HashMap;
 
-pub struct NumericIndex(sled::Tree);
+/// Converts the number into a big-endian value which is suitable for querying/storing in sled
+fn encoded_number(n: u64) -> Vec<u8> {
+    n.to_be_bytes().to_vec()
+}
 
+pub struct NumericIndex(sled::Tree);
 impl NumericIndex {
     pub fn open(db: &Db, name: &str) -> Self {
         let tree = db
@@ -14,34 +18,56 @@ impl NumericIndex {
     }
 
     pub fn upsert(&self, point_id: u64, value: i64) -> Result<(), sled::Error> {
-        let mut key_bytes = Vec::new();
-        // key_bytes.write_u64(point_id).await.unwrap(); // avoid async write:
-        key_bytes.extend_from_slice(&point_id.to_be_bytes());
-        self.0
-            .insert(key_bytes, &value.to_be_bytes())
+        // In numeric tree, the point value becomes tree's key, and the point ID is part of a list of values.
+        let tree_key = encoded_number(value as u64);
+
+        // Fetch existing point IDs for this value
+        let mut point_ids: Vec<u64> = self
+            .0
+            .get(&tree_key)?
+            .map(|data| {
+                let (decoded, cnt) =
+                    bincode::decode_from_slice(&data, bincode::config::standard()).unwrap();
+                println!("Decoded point IDs: {:?} with count {}", decoded, cnt);
+                decoded
+            })
+            .unwrap_or_default();
+
+        // Add point and sort
+        point_ids.push(point_id);
+        point_ids.sort();
+
+        // Store back
+        let serialized_ids = bincode::encode_to_vec(point_ids, bincode::config::standard())
             .map_err(|e| {
-                sled::Error::Io(std::io::Error::other(format!(
-                    "Failed to insert into index tree: {e}"
-                )))
+                sled::Error::Io(std::io::Error::other(format!("Failed to encode: {e}")))
             })?;
+
+        self.0.insert(&tree_key, serialized_ids).map_err(|e| {
+            sled::Error::Io(std::io::Error::other(format!(
+                "Failed to insert into index tree: {e}"
+            )))
+        })?;
 
         Ok(())
     }
 
     pub fn query_gte(&self, value: i64) -> Result<Vec<PointId>, sled::Error> {
-        let mut min_key = Vec::new();
-        min_key.extend_from_slice(&value.to_be_bytes());
+        let min_key = encoded_number(value as u64);
 
         let mut results = Vec::new();
-        // let mut item = self.0.range(min_key..);
         for item in self.0.range(min_key..) {
-            let (_key, _value) = item?;
-            // let point_id = u64::from_be_bytes(value.to_vec());
-            let point_id = 0;
-            results.push(PointId::Id(point_id));
+            let (_gte_value, encoded_point_ids) = item?;
+            let point_ids: Vec<u64> =
+                bincode::decode_from_slice(&encoded_point_ids, bincode::config::standard())
+                    .map_err(|e| {
+                        sled::Error::Io(std::io::Error::other(format!("Failed to decode: {e}")))
+                    })?
+                    .0;
+            results.extend_from_slice(&point_ids);
         }
 
-        Ok(results)
+        Ok(results.into_iter().map(PointId::Id).collect())
     }
 }
 
@@ -55,14 +81,7 @@ impl FieldIndex {
         FieldIndex::Numeric(NumericIndex::open(db, name))
     }
 
-    pub fn add_point(&self, point_id: PointId, value: &Value) -> Result<(), sled::Error> {
-        let PointId::Id(point_id) = point_id else {
-            return Err(sled::Error::Io(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                "Invalid PointId type",
-            )));
-        };
-
+    pub fn add_point(&self, point_id: u64, value: &Value) -> Result<(), sled::Error> {
         match self {
             FieldIndex::Numeric(index) => match value {
                 Value::Number(num) if num.is_i64() => {
@@ -96,8 +115,8 @@ impl PayloadIndex {
         Self { indices }
     }
 
-    pub fn get_index_names(&self) -> Vec<String> {
-        self.indices.keys().cloned().collect()
+    pub fn get_index_names(&self) -> Vec<&str> {
+        self.indices.keys().map(|k| k.as_str()).collect()
     }
 
     pub fn load(db: &Db) -> Self {
@@ -106,9 +125,16 @@ impl PayloadIndex {
     }
 
     pub fn upsert(&self, point: &Point) -> Result<(), sled::Error> {
+        let PointId::Id(point_id) = point.id else {
+            return Err(sled::Error::Io(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Invalid PointId type",
+            )));
+        };
+
         for (index_key, index_tree) in &self.indices {
             if let Some(payload_value) = point.payload.get(index_key) {
-                index_tree.add_point(point.id.clone(), payload_value)?;
+                index_tree.add_point(point_id, payload_value)?;
             }
         }
         Ok(())
@@ -142,6 +168,7 @@ mod test {
         if let FieldIndex::Numeric(numeric_index) = field_index {
             let results = numeric_index.query_gte(40).unwrap();
             assert_eq!(results.len(), 6); // Points with ids 4, 5, 6, 7, 8, 9
+            assert_eq!(results, (4..10).map(PointId::Id).collect::<Vec<_>>());
         } else {
             panic!("Expected NumericIndex");
         }
