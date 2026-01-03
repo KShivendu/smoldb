fcfe1319e130ea0fd80bc55fdf113230636f8079
KShivendu <kshivendu1@gmail.com>
2026-01-04 01:23:53 +0530

    Initialize once in benches

--- benches/collection/read.rs
+++ benches/collection/read.rs
@@ -11,12 +11,12 @@ use crate::common::{
 
 pub fn read(c: &mut Criterion) {
     let mut group = benchmark_group(c, "read");
+    let rt = create_runtime();
 
     // Perf in the beginning: ???
     // Perf with hashring and tokio: 729.73 ns
     // Read a single point when there are NUM_POINTS points in the collection
-    group.bench_function("single", |b| {
-        let rt = create_runtime();
+    {
         let tempdir = create_tempdir();
         let channel_service = create_channel_service();
         let collection = rt.block_on(async {
@@ -28,19 +28,19 @@ pub fn read(c: &mut Criterion) {
             )
             .await
         });
-        b.to_async(&rt).iter(|| async {
-            collection
-                .read_points(Some(vec![PointId::Id(NUM_POINTS - 1)]), None, true)
-                .await
-                .unwrap();
-        });
-    });
+
+        group.bench_function("single", |b| {
+            b.to_async(&rt).iter(|| async {
+                collection
+                    .read_points(Some(vec![PointId::Id(NUM_POINTS - 1)]), None, true)
+                    .await
+                    .unwrap();
+            });
+        })
+    };
 
     // Read 1 batch of BATCH_SIZE points while there are NUM_POINTS points in the collection
-    group.throughput(Throughput::Elements(BATCH_SIZE as u64));
-    group.bench_function("batch", |b| {
-        let rt = create_runtime();
-
+    {
         let tempdir = create_tempdir();
         let channel_service = create_channel_service();
         let collection = rt.block_on(async {
@@ -56,44 +56,49 @@ pub fn read(c: &mut Criterion) {
             .map(|i| PointId::Id(i as u64))
             .collect::<Vec<_>>();
 
-        b.to_async(&rt).iter(|| async {
-            collection
-                .read_points(Some(read_batch.clone()), None, true)
-                .await
-                .unwrap();
-        });
-    });
+        group.throughput(Throughput::Elements(BATCH_SIZE as u64));
+        group.bench_function("batch", |b| {
+            b.to_async(&rt).iter(|| async {
+                collection
+                    .read_points(Some(read_batch.clone()), None, true)
+                    .await
+                    .unwrap();
+            });
+        })
+    };
 
     // Perf in the beginning: ???
     // Perf with hashring and tokio: 124.54ms (100_000 points, 4 threads, 2 shards; only 170x slower than single read)
     // Read NUM_POINTS points in NUM_THREADS parallel batches of BATCH_SIZE points
-    group.throughput(Throughput::Elements(NUM_POINTS));
-    group.bench_function("concurrent", |b| {
-        let rt = create_runtime();
-
+    {
         let tempdir = create_tempdir();
-        let channel_service = create_channel_service();
+        let channel_service: Arc<smoldb::channel_service::ChannelService> =
+            create_channel_service();
         let points = generate_points(NUM_POINTS);
         let all_point_ids = points.iter().map(|p| p.id.clone()).collect::<Vec<_>>();
         let collection = rt.block_on(async {
             create_collection_with_points(&tempdir, channel_service, None, points).await
         });
         let collection_arc = Arc::new(collection);
-        b.to_async(&rt).iter(|| async {
-            stream::iter(all_point_ids.chunks(BATCH_SIZE))
-                .map(|chunk| {
-                    let collection_clone = collection_arc.clone();
-                    let chunk = chunk.to_vec();
-                    async move {
-                        collection_clone
-                            .read_points(Some(chunk), None, true)
-                            .await
-                            .unwrap()
-                    }
-                })
-                .buffer_unordered(CONCURRENCY)
-                .collect::<Vec<_>>()
-                .await;
+
+        group.throughput(Throughput::Elements(NUM_POINTS));
+        group.bench_function("concurrent", |b| {
+            b.to_async(&rt).iter(|| async {
+                stream::iter(all_point_ids.chunks(BATCH_SIZE))
+                    .map(|chunk| {
+                        let collection_clone = collection_arc.clone();
+                        let chunk = chunk.to_vec();
+                        async move {
+                            collection_clone
+                                .read_points(Some(chunk), None, true)
+                                .await
+                                .unwrap()
+                        }
+                    })
+                    .buffer_unordered(CONCURRENCY)
+                    .collect::<Vec<_>>()
+                    .await;
+            });
         });
-    });
+    }
 }
