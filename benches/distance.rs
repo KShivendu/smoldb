c6b767d47b08ca3c4b9cd0dc052b60e46d12b77a
Kumar Shivendu <kshivendu1@gmail.com>
2026-01-14 10:56:08 +0530

    Add benchmark for vector distance calculation (#109)
    * Add benchmark for vector distance calculation
    
    * Use 1536 dims
    
    * Avoid checks in cosine similarity func
    
    * no unwrap

---
+++ benches/distance.rs
@@ -0,0 +1,27 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use rand::{rngs::StdRng, Rng, SeedableRng};
+use smoldb::storage::index::vector::cosine_similarity;
+
+fn distance_bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("distance");
+
+    let mut rng = StdRng::seed_from_u64(42);
+
+    let dim = 1536;
+
+    let random_vectors1: Vec<f64> = (0..dim).map(|_| rng.random::<f64>()).collect::<Vec<_>>();
+    let random_vectors2: Vec<f64> = (0..dim).map(|_| rng.random::<f64>()).collect::<Vec<_>>();
+
+    group.bench_function("cosine_similarity", |b| {
+        b.iter(|| {
+            cosine_similarity(&random_vectors1, &random_vectors2);
+        });
+    });
+}
+
+criterion_group!(
+    name = benches;
+    config = Criterion::default();
+    targets = distance_bench
+);
+criterion_main!(benches);
